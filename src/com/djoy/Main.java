292589ae6a5fdca5cbf0a3fbfa8b51581d838960
MrDjoy <1023747194@qq.com>
2016-07-12 13:36:47 +0800

    test git

---
+++ src/com/djoy/Main.java
@@ -0,0 +1,12 @@
+package com.djoy;
+
+/**
+ * Created by Djoy on 2016/7/11.
+ */
+public class Main {
+    public static  void  main(String[] args){
+        System.out.print("hello world");
+        System.out.println("hello world");
+    }
+}
+
